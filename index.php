903b4b0683934534496ba068f878f9fd99f81af7
malado1's <arman.papiants@yandex.ru>
2023-11-03 09:59:40 +0300

    введение_1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<meta charset="UTF-8">
+<title>Программирование на языке PHP</title>
+<body>
+    <h1>Введение в PHP</h1>
+    <h2>Первая программа PHP</h2>
+    <?php
+    echo "Hello, User!";
+    ?>
+</body>
+</html>
